dbc47d65425f3cb8876133cb7d3d4f9af8c25b8d
Subhan Khan <subhanism@outlook.com>
2026-06-01 23:47:15 +0500

    changes added

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import spacy
-
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -18,5 +18,7 @@ def predict():
 
     return jsonify(entities)
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
